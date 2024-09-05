95b54a3a124670977b36c8abf03c02cbb812e861
Ildar Talipov <ildar.talipov.00@mail.ru>
2024-09-05 06:23:42 +0300

    first

---
+++ apps/account/src/app/auth/auth.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@nestjs/common';
+import {RegisterDto} from "./auth.controller";
+import {UserRepository} from "../user/repositories/user.repositories";
+import {UserEntity} from "../user/entities/user.entity";
+import {UserRole} from "@microservices/interfaces";
+import {JwtService} from "@nestjs/jwt";
+import {AccountRegister} from "@microservices/contracts";
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly jwtService: JwtService
+    ) {}
+  async register({email, password, displayName}: AccountRegister.Request) {
+    const oldUser = await this.userRepository.findUser(email)
+    if(oldUser) {
+      throw new Error('Такой пользователь уже зарегистрирован')
+    }
+
+    const newUserEntity = await new UserEntity({
+      displayName,
+      email,
+      role: UserRole.Student,
+      passwordHash: ''
+      }).setPassword(password);
+    const newUser = await this.userRepository.createUser(newUserEntity);
+    return {email: newUser.email}
+  }
+
+  async validateUser(email:string, password:string) {
+    const user = await this.userRepository.findUser(email);
+    if(!user) {
+      throw new Error('Неверный логин и пароль')
+    }
+
+    const userEntity = new UserEntity(user);
+    const isCorrectPassword = await userEntity.validatePassword(password)
+    if (!isCorrectPassword) {
+      throw new Error('Неверный логин и пароль')
+    }
+
+    return {id:user._id}
+  }
+
+  async login(id:string) {
+    return {
+      access_token: await this.jwtService.signAsync({id})
+    }
+  }
+}
